1602661afb8731aea29d0291ddd0aa53b867204e
Bawantha <bawanthapasqual@gmail.com>
2024-12-28 11:35:54 +0530

    fixed error db

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const uri = process.env.MONGODB_URI;
 let dbClient;
 let usersCollection;
 let categoriesCollection;
-let productCollection;
+let productCollection
 
 async function connectToDatabase() {
   try {
@@ -18,14 +18,9 @@ async function connectToDatabase() {
         deprecationErrors: true,
       },
     });
-    
-    // Establish the database connection
+
     await dbClient.connect();
-
-    // Select the database
     const ecommercedb = dbClient.db("E-Commerce");
-
-    // Initialize collections
     usersCollection = ecommercedb.collection("users");
     categoriesCollection = ecommercedb.collection("categories");
     productCollection = ecommercedb.collection("products");
@@ -33,44 +28,24 @@ async function connectToDatabase() {
     console.log("Successfully connected to MongoDB!");
   } catch (error) {
     console.error("Failed to connect to MongoDB", error);
-    throw new Error("Database connection failed");
   }
 }
 
 function getUsersCollection() {
-  if (!usersCollection) {
-    throw new Error("Database not initialized. Call connectToDatabase first.");
-  }
   return usersCollection;
 }
 
 function getCategoriesCollection() {
-  if (!categoriesCollection) {
-    throw new Error("Database not initialized. Call connectToDatabase first.");
-  }
   return categoriesCollection;
 }
 
-function getProductCollection() {
-  if (!productCollection) {
-    throw new Error("Database not initialized. Call connectToDatabase first.");
-  }
+function getproductCollection() {
   return productCollection;
-}
-
-async function closeDatabaseConnection() {
-  if (dbClient) {
-    await dbClient.close();
-    console.log("Database connection closed");
-  } else {
-    console.warn("Database connection is not open");
-  }
 }
 
 module.exports = {
   connectToDatabase,
   getUsersCollection,
   getCategoriesCollection,
-  getProductCollection,
-  closeDatabaseConnection,
+  getproductCollection
 };
