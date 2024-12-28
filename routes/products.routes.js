10c98488b4c20c45c8443aadd937998d0f8cfec4
Bawantha <bawanthapasqual@gmail.com>
2024-12-28 09:59:45 +0530

    count route

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -4,11 +4,16 @@ const verifyToken = require("../middleware/auth.middleware");
 
 const router = express.Router();
 
-router.post("/", verifyToken , productController.addProduct);
+router.post("/", verifyToken, productController.addProduct);
 router.get("/", verifyToken, productController.getAllProducts);
-router.get("/minimumQuantity", verifyToken , productController.getMinimumQuantityProducts);
-router.get("/:id", verifyToken ,productController.getProductById);
-router.put("/:id",verifyToken,  productController.updateProduct);
-router.delete("/:id", verifyToken , productController.deleteProduct);
+router.get("/count", verifyToken, productController.getProductCount);
+router.get(
+  "/minimumQuantity",
+  verifyToken,
+  productController.getMinimumQuantityProducts
+);
+router.get("/:id", verifyToken, productController.getProductById);
+router.put("/:id", verifyToken, productController.updateProduct);
+router.delete("/:id", verifyToken, productController.deleteProduct);
 
 module.exports = router;
