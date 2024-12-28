8757b7d1fdf7783359ec0c18df0d45d3fb275aad
Bawantha <bawanthapasqual@gmail.com>
2024-12-28 11:13:03 +0530

    change port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const routes = require("./routes/index.routes");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
